dfbb133dc56a0486428d88c32349ee2ae86b103a
Vadym <dastus44@gmail.com>
2016-03-18 10:26:55 +0200

    12.1 fixes

--- students/forms.py
+++ students/forms.py
@@ -6,6 +6,6 @@ from django.forms import ModelForm
 class StudentModelForm(ModelForm):
     class Meta:
         model = Student
-        #fields = ['name', 'surname', 'date_of_birth', 'email', 'phone', 'address', 'skype', 'courses']
+        fields = ['name', 'surname', 'date_of_birth', 'email', 'phone', 'address', 'skype', 'courses']
         #fields = ['name', 'surname', 'date_of_birth', 'email', 'phone', 'address', 'skype']
         #help_texts={'courses':"Hold down \"Control\", or \"Command\" on a Mac, to select more than one1."}
