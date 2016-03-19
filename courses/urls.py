2b7c042c6c78b4ba169a7cc4cfba61dbc84c2bb0
Vadym <dastus44@gmail.com>
2016-03-19 23:12:42 +0200

    12.2 url update

--- courses/urls.py
+++ courses/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url((r'^add/$'), views.add, name='add'),
     url((r'^edit/(?P<course_id>\d+)/$'), views.edit, name='edit'),
     url((r'^remove/(?P<course_id>\d+)/$'), views.remove, name='remove'),
-    url((r'^(?P<course_id>\d+)/add_lesson/$'), views.add_lesson, name='add_lesson'),
+    url((r'^(?P<course_id>\d+)/add_lesson$'), views.add_lesson, name='add-lesson'),
+    url((r'^(?P<course_id>\d+)/add_lesson/$'), views.add_lesson, name='add-lesson'),
 ]
